2ce8127bbccca49045d3f9d4cd988bda86222afd
Ronald  Luo <cytochrome@Ronalds-Air.ht.home>
2022-12-19 00:41:00 -0500

    create media server

---
+++ README.md
@@ -0,0 +1,21 @@
+## Media-Server
+
+# What it does:
+
+Stream videos from your machine to any device on your network
+
+# Start Server:
+
+```bash
+git clone https://github.com/ronald-luo/media-server.git
+cd media-server
+npm install
+node app.js
+```
+
+# How to use it:
+
+1. Install the latest version of node (if you don't have it).
+1. Upload video files into the media provided folder.
+2. Run the server.
+3. Access videos at http://www.<your_ip_address>/video/<name_of_video>
---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { video: req.params.id });
+});
+
+/* GET video listing. */
+router.get('/video/:id', function(req, res) {
+  console.log(req.params.id)
+  const path = `./media/${req.params.id}.mp4`;
+  console.log(req.body.video)
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  if (range) {
+    console.log('we have range', range);
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+      console.log(parts)
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    console.log('no range', range);
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+});
+
+module.exports = router;
